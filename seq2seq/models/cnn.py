ae5c5512941b14179ad4ae7465c8bcf4836d78b5
LeHuuLoi <lehuuloi.cs@gmail.com>
2023-06-10 15:52:07 +0700

    update

--- seq2seq/models/cnn.py
+++ seq2seq/models/cnn.py
@@ -101,7 +101,7 @@ def extend_conv_spec(convolutions):
 
 class Encoder(nn.Module):
     """Encoder"""
-    def __init__(self, vocabulary, device, embed_dim=256, convolutions=((256, 3),) * 3,
+    def __init__(self, vocabulary, device, embed_dim=512, convolutions=((512, 3),) * 3,
                  dropout=0.5, max_positions=5000):
         super().__init__()
         self.vocabulary = vocabulary
@@ -241,7 +241,7 @@ class Attention(nn.Module):
         return attended_combined, attention
 class Decoder(nn.Module):
     """Decoder"""
-    def __init__(self, vocabulary, device, embed_dim=256, convolutions=((256, 3),) * 3,
+    def __init__(self, vocabulary, device, embed_dim=512, convolutions=((512, 3),) * 3,
                  dropout=0.5, max_positions=5000):
         super().__init__()
 
