e90c2fbf9e88cfbb44ac719d0ace13e4241ab758
lam245 <nguyenxuanlamnghean123@gmail.com>
2023-11-25 00:18:40 +0700

    sasssasaaadssaasaaaaddsa

--- parser_data/load_data.py
+++ parser_data/load_data.py
@@ -3,6 +3,10 @@ import tqdm
 import pandas as pd
 
 from datasets import Dataset
+import json
+from tqdm import tqdm
+from datasets import Dataset
+
 def load_json(data_path: str):
     with open(data_path) as f:
         data_list = json.load(f)
@@ -11,7 +15,7 @@ def load_json(data_path: str):
     questions = []
     answers = []
 
-    for data in tqdm(data_list):  # Use tqdm as a function, not a module
+    for data in tqdm(data_list, desc="Processing data"):  # Use tqdm as a function, not a module
         context = data.get("input", "")
         instruction = data.get("instruction", "")
         if "output" in data:
