88155bc056b31f7f2ea7dc923f500c88a6a0b994
lam245 <nguyenxuanlamnghean123@gmail.com>
2023-11-24 23:59:45 +0700

    sasssasaaadssaa

--- parser_data/load_data.py
+++ parser_data/load_data.py
@@ -7,10 +7,12 @@ def load_json(data_path: str):
     with open(data_path) as f:
         data_list = json.load(f)
 
+    contexts = []
     questions = []
     answers = []
 
     for data in tqdm(data_list):
+        context = data.get("input", "")
         instruction = data.get("instruction", "")
         if "output" in data:
             output_text = data["output"]
@@ -18,9 +20,10 @@ def load_json(data_path: str):
             for qa_pair in qa_pairs:
                 if len(qa_pair) == 2:
                     question, answer = qa_pair
+                    contexts.append(context)
                     questions.append(question)
                     answers.append(answer)
 
-    dict_obj = {'questions': questions, 'answers': answers}
+    dict_obj = {'contexts': contexts, 'questions': questions, 'answers': answers}
     datasets = Dataset.from_dict(dict_obj)
     return datasets
