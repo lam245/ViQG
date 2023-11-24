bc005907a188d3c17ff0a98d61cfcab8a52d8135
lam245 <nguyenxuanlamnghean123@gmail.com>
2023-11-24 20:13:40 +0700

    refact co

--- parser_data/load_data.py
+++ parser_data/load_data.py
@@ -1,46 +1,33 @@
 import json
 import tqdm
+import pandas as pd
+
 from datasets import Dataset
-def load_json(data_path:str, dataset:str):
+def load_json(data_path: str, dataset: str):
     questions = []
     contexts, answers = [], []
-    if dataset in ['ViNewsQA','ViQuAD']:
-        with open(data_path) as f:
-            data = json.load(f)
-            del data["version"]
-        for i in tqdm.tqdm(range(len(data['data']))):
-            for paragraph in data['data'][i]['paragraphs']:
-                for qa in paragraph['qas']:
-                    question = qa['question']
-                    context = paragraph['context']
-                    answer = qa['answers'][0]['text']
-                    answers.append(answer)
-                    contexts.append(context)
-                    questions.append(question)
-    elif dataset in ['ViMMRC1.0','ViMMRC2.0']:
-        with open(data_path) as f:
-            data_list = json.load(f)
-        for data in tqdm.tqdm(data_list):
-            article = data['article']
-            questions_list = data['questions']
-            options_list = data['options']
-            answers_list = data['answers']
 
-            for i, question in enumerate(questions_list):
-                answer = options_list[i][ord(answers_list[i]) - 65]
-                questions.append(question)
-                contexts.append(article)
-                answers.append(answer)
-    else:
-        with open(data_path) as f:
-            data = json.load(f)
-            del data["version"]
-        for i in tqdm.tqdm(range(len(data['data']))):
-            for question in data['data'][i]['questions']:
-                questions.append(question['input_text'])
-                contexts.append(data['data'][i]['story'])
-            for answer in data['data'][i]['answers']:
-                answers.append(answer['input_text'])
-    dict_obj = {'contexts':contexts, 'answers':answers, 'questions':questions}
-    datasets = Dataset.from_dict(dict_obj)
+    if dataset in ['ViNewsQA', 'ViQuAD', 'ViMMRC1.0', 'ViMMRC2.0']:
+        df = pd.read_parquet(data_path)
+
+        for i in range(len(df)):
+            if dataset in ['ViNewsQA', 'ViQuAD']:
+                question = df.loc[i, 'question']
+                context = df.loc[i, 'context']
+                answer = df.loc[i, 'answers']
+            elif dataset in ['ViMMRC1.0', 'ViMMRC2.0']:
+                question = df.loc[i, 'question']
+                context = df.loc[i, 'article']
+                answer = df.loc[i, 'answers']
+            else:
+                question = df.loc[i, 'input_text']
+                context = df.loc[i, 'story']
+                answer = df.loc[i, 'input_text']  # Assuming the answer is in the same column
+
+            questions.append(question)
+            contexts.append(context)
+            answers.append(answer)
+
+    dict_obj = {'contexts': contexts, 'answers': answers, 'questions': questions}
+    datasets = Dataset.from_dict(dict_obj)  # Assuming you have a Dataset class or function
     return datasets
